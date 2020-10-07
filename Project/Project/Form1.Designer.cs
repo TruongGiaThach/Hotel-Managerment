773d333cab77c88fa72a2ec8dd90508bda2c7201
Dung-kun <19521399@gm.uit.edu.vn>
2020-10-07 08:41:47 +0700

    Dungkun

--- Project/Project/Form1.Designer.cs
+++ Project/Project/Form1.Designer.cs
@@ -28,13 +28,300 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gradientPanel1 = new Syncfusion.Windows.Forms.Tools.GradientPanel();
+            this.gradientPanel2 = new Syncfusion.Windows.Forms.Tools.GradientPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.autoLabel1 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
+            this.autoLabel2 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
+            this.autoLabel3 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
+            this.autoLabel4 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
+            this.autoLabel5 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
+            this.autoLabel6 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
+            this.autoLabel7 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
+            this.First_name = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
+            this.Last_name = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
+            this.Email = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
+            this.Phone = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
+            this._Account = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
+            this._Password = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
+            this._Repassword = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
+            this._Sign_up = new MaterialSkin.Controls.MaterialButton();
+            this.gradientPanel3 = new Syncfusion.Windows.Forms.Tools.GradientPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.gradientPanel1)).BeginInit();
+            this.gradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gradientPanel2)).BeginInit();
+            this.gradientPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.First_name)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Last_name)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Email)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Phone)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._Account)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._Password)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._Repassword)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gradientPanel3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gradientPanel1
+            // 
+            this.gradientPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gradientPanel1.BackgroundColor = new Syncfusion.Drawing.BrushInfo(Syncfusion.Drawing.GradientStyle.ForwardDiagonal, System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(255)))), ((int)(((byte)(218))))), System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(0)))), ((int)(((byte)(234))))));
+            this.gradientPanel1.Border3DStyle = System.Windows.Forms.Border3DStyle.Adjust;
+            this.gradientPanel1.Controls.Add(this.gradientPanel2);
+            this.gradientPanel1.Location = new System.Drawing.Point(0, 23);
+            this.gradientPanel1.Name = "gradientPanel1";
+            this.gradientPanel1.Size = new System.Drawing.Size(894, 545);
+            this.gradientPanel1.TabIndex = 0;
+            // 
+            // gradientPanel2
+            // 
+            this.gradientPanel2.BackgroundColor = new Syncfusion.Drawing.BrushInfo(Syncfusion.Drawing.GradientStyle.ForwardDiagonal, System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(79)))), ((int)(((byte)(254))))), System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(255)))), ((int)(((byte)(255))))));
+            this.gradientPanel2.Border3DStyle = System.Windows.Forms.Border3DStyle.Adjust;
+            this.gradientPanel2.Controls.Add(this.gradientPanel3);
+            this.gradientPanel2.Controls.Add(this._Sign_up);
+            this.gradientPanel2.Controls.Add(this._Repassword);
+            this.gradientPanel2.Controls.Add(this._Password);
+            this.gradientPanel2.Controls.Add(this._Account);
+            this.gradientPanel2.Controls.Add(this.Phone);
+            this.gradientPanel2.Controls.Add(this.Email);
+            this.gradientPanel2.Controls.Add(this.Last_name);
+            this.gradientPanel2.Controls.Add(this.First_name);
+            this.gradientPanel2.Controls.Add(this.autoLabel7);
+            this.gradientPanel2.Controls.Add(this.autoLabel6);
+            this.gradientPanel2.Controls.Add(this.autoLabel5);
+            this.gradientPanel2.Controls.Add(this.autoLabel4);
+            this.gradientPanel2.Controls.Add(this.autoLabel3);
+            this.gradientPanel2.Controls.Add(this.autoLabel2);
+            this.gradientPanel2.Controls.Add(this.autoLabel1);
+            this.gradientPanel2.Controls.Add(this.label1);
+            this.gradientPanel2.Location = new System.Drawing.Point(160, 86);
+            this.gradientPanel2.Name = "gradientPanel2";
+            this.gradientPanel2.Size = new System.Drawing.Size(570, 326);
+            this.gradientPanel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Create new account";
+            // 
+            // autoLabel1
+            // 
+            this.autoLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.autoLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.autoLabel1.ForeColor = System.Drawing.Color.White;
+            this.autoLabel1.Location = new System.Drawing.Point(247, 72);
+            this.autoLabel1.Name = "autoLabel1";
+            this.autoLabel1.Size = new System.Drawing.Size(65, 13);
+            this.autoLabel1.TabIndex = 1;
+            this.autoLabel1.Text = "First name";
+            // 
+            // autoLabel2
+            // 
+            this.autoLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.autoLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.autoLabel2.ForeColor = System.Drawing.Color.White;
+            this.autoLabel2.Location = new System.Drawing.Point(387, 72);
+            this.autoLabel2.Name = "autoLabel2";
+            this.autoLabel2.Size = new System.Drawing.Size(65, 13);
+            this.autoLabel2.TabIndex = 2;
+            this.autoLabel2.Text = "Last name";
+            // 
+            // autoLabel3
+            // 
+            this.autoLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.autoLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.autoLabel3.ForeColor = System.Drawing.Color.White;
+            this.autoLabel3.Location = new System.Drawing.Point(247, 112);
+            this.autoLabel3.Name = "autoLabel3";
+            this.autoLabel3.Size = new System.Drawing.Size(37, 13);
+            this.autoLabel3.TabIndex = 3;
+            this.autoLabel3.Text = "Email";
+            // 
+            // autoLabel4
+            // 
+            this.autoLabel4.BackColor = System.Drawing.Color.Transparent;
+            this.autoLabel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.autoLabel4.ForeColor = System.Drawing.Color.White;
+            this.autoLabel4.Location = new System.Drawing.Point(387, 115);
+            this.autoLabel4.Name = "autoLabel4";
+            this.autoLabel4.Size = new System.Drawing.Size(72, 13);
+            this.autoLabel4.TabIndex = 4;
+            this.autoLabel4.Text = "Your phone";
+            // 
+            // autoLabel5
+            // 
+            this.autoLabel5.AutoSize = false;
+            this.autoLabel5.BackColor = System.Drawing.Color.Transparent;
+            this.autoLabel5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.autoLabel5.ForeColor = System.Drawing.Color.White;
+            this.autoLabel5.Location = new System.Drawing.Point(247, 154);
+            this.autoLabel5.Name = "autoLabel5";
+            this.autoLabel5.Size = new System.Drawing.Size(60, 16);
+            this.autoLabel5.TabIndex = 5;
+            this.autoLabel5.Text = "Account";
+            // 
+            // autoLabel6
+            // 
+            this.autoLabel6.BackColor = System.Drawing.Color.Transparent;
+            this.autoLabel6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.autoLabel6.ForeColor = System.Drawing.Color.White;
+            this.autoLabel6.Location = new System.Drawing.Point(247, 196);
+            this.autoLabel6.Name = "autoLabel6";
+            this.autoLabel6.Size = new System.Drawing.Size(61, 13);
+            this.autoLabel6.TabIndex = 6;
+            this.autoLabel6.Text = "Password";
+            // 
+            // autoLabel7
+            // 
+            this.autoLabel7.BackColor = System.Drawing.Color.Transparent;
+            this.autoLabel7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.autoLabel7.ForeColor = System.Drawing.Color.White;
+            this.autoLabel7.Location = new System.Drawing.Point(247, 235);
+            this.autoLabel7.Name = "autoLabel7";
+            this.autoLabel7.Size = new System.Drawing.Size(76, 13);
+            this.autoLabel7.TabIndex = 7;
+            this.autoLabel7.Text = "Repassword";
+            // 
+            // First_name
+            // 
+            this.First_name.BeforeTouchSize = new System.Drawing.Size(278, 20);
+            this.First_name.Location = new System.Drawing.Point(247, 89);
+            this.First_name.Name = "First_name";
+            this.First_name.Size = new System.Drawing.Size(134, 20);
+            this.First_name.TabIndex = 8;
+            // 
+            // Last_name
+            // 
+            this.Last_name.BeforeTouchSize = new System.Drawing.Size(278, 20);
+            this.Last_name.Location = new System.Drawing.Point(387, 88);
+            this.Last_name.Name = "Last_name";
+            this.Last_name.Size = new System.Drawing.Size(138, 20);
+            this.Last_name.TabIndex = 9;
+            // 
+            // Email
+            // 
+            this.Email.BeforeTouchSize = new System.Drawing.Size(278, 20);
+            this.Email.Location = new System.Drawing.Point(247, 131);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(134, 20);
+            this.Email.TabIndex = 10;
+            // 
+            // Phone
+            // 
+            this.Phone.BeforeTouchSize = new System.Drawing.Size(278, 20);
+            this.Phone.Location = new System.Drawing.Point(387, 131);
+            this.Phone.Name = "Phone";
+            this.Phone.Size = new System.Drawing.Size(138, 20);
+            this.Phone.TabIndex = 11;
+            // 
+            // _Account
+            // 
+            this._Account.BeforeTouchSize = new System.Drawing.Size(278, 20);
+            this._Account.Location = new System.Drawing.Point(247, 173);
+            this._Account.Name = "_Account";
+            this._Account.Size = new System.Drawing.Size(278, 20);
+            this._Account.TabIndex = 12;
+            // 
+            // _Password
+            // 
+            this._Password.BeforeTouchSize = new System.Drawing.Size(278, 20);
+            this._Password.Location = new System.Drawing.Point(247, 212);
+            this._Password.Name = "_Password";
+            this._Password.Size = new System.Drawing.Size(278, 20);
+            this._Password.TabIndex = 13;
+            // 
+            // _Repassword
+            // 
+            this._Repassword.BeforeTouchSize = new System.Drawing.Size(278, 20);
+            this._Repassword.Location = new System.Drawing.Point(247, 251);
+            this._Repassword.Name = "_Repassword";
+            this._Repassword.Size = new System.Drawing.Size(278, 20);
+            this._Repassword.TabIndex = 14;
+            // 
+            // _Sign_up
+            // 
+            this._Sign_up.AutoSize = false;
+            this._Sign_up.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this._Sign_up.Depth = 0;
+            this._Sign_up.DrawShadows = true;
+            this._Sign_up.HighEmphasis = true;
+            this._Sign_up.Icon = null;
+            this._Sign_up.Location = new System.Drawing.Point(247, 280);
+            this._Sign_up.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this._Sign_up.MouseState = MaterialSkin.MouseState.HOVER;
+            this._Sign_up.Name = "_Sign_up";
+            this._Sign_up.Size = new System.Drawing.Size(278, 23);
+            this._Sign_up.TabIndex = 15;
+            this._Sign_up.Text = "SIGN UP";
+            this._Sign_up.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this._Sign_up.UseAccentColor = false;
+            this._Sign_up.UseVisualStyleBackColor = true;
+            // 
+            // gradientPanel3
+            // 
+            this.gradientPanel3.BackColor = System.Drawing.Color.Transparent;
+            this.gradientPanel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("gradientPanel3.BackgroundImage")));
+            this.gradientPanel3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.gradientPanel3.Location = new System.Drawing.Point(49, 89);
+            this.gradientPanel3.Name = "gradientPanel3";
+            this.gradientPanel3.Size = new System.Drawing.Size(130, 131);
+            this.gradientPanel3.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(892, 529);
+            this.Controls.Add(this.gradientPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gradientPanel1)).EndInit();
+            this.gradientPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gradientPanel2)).EndInit();
+            this.gradientPanel2.ResumeLayout(false);
+            this.gradientPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.First_name)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Last_name)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Email)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Phone)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._Account)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._Password)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._Repassword)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gradientPanel3)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Syncfusion.Windows.Forms.Tools.GradientPanel gradientPanel1;
+        private Syncfusion.Windows.Forms.Tools.GradientPanel gradientPanel2;
+        private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel5;
+        private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel4;
+        private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel3;
+        private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel2;
+        private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel1;
+        private System.Windows.Forms.Label label1;
+        private Syncfusion.Windows.Forms.Tools.TextBoxExt _Repassword;
+        private Syncfusion.Windows.Forms.Tools.TextBoxExt _Password;
+        private Syncfusion.Windows.Forms.Tools.TextBoxExt _Account;
+        private Syncfusion.Windows.Forms.Tools.TextBoxExt Phone;
+        private Syncfusion.Windows.Forms.Tools.TextBoxExt Email;
+        private Syncfusion.Windows.Forms.Tools.TextBoxExt Last_name;
+        private Syncfusion.Windows.Forms.Tools.TextBoxExt First_name;
+        private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel7;
+        private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel6;
+        private Syncfusion.Windows.Forms.Tools.GradientPanel gradientPanel3;
+        private MaterialSkin.Controls.MaterialButton _Sign_up;
     }
 }
 
