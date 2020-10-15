a2fefd2bd4370d28addcc50949fce5f93f3fefbb
NguyenTanTien2001 <71328979+NguyenTanTien2001@users.noreply.github.com>
2020-10-15 13:34:06 +0700

    add Home

---
+++ Project/Project/TrangChu_MDIc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class TrangChu_MDIc : Form
+    {
+        public TrangChu_MDIc()
+        {
+            InitializeComponent();
+            InitialPictureSlide();
+        }
+
+        #region 
+        private void timerPictureSlide_Tick(object sender, EventArgs e)
+        {
+            //PictureSlideIndex++;
+            //if (PictureSlideIndex == imageSlideList.Images.Count) PictureSlideIndex = 0;
+            //pictureSlideBox.Image = imageSlideList.Images[PictureSlideIndex];
+        }
+        #endregion
+    }
+}
