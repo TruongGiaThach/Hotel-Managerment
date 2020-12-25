b134943509221ac1079e3ca0b83335bf05187789
NguyenTanTien2001 <71328979+NguyenTanTien2001@users.noreply.github.com>
2020-12-25 21:03:34 +0700

    update

--- HotelBookingManagement/HotelBookingManagement/Presentation Layers/Reset_Pass_Form.Designer.cs
+++ HotelBookingManagement/HotelBookingManagement/Presentation Layers/Reset_Pass_Form.Designer.cs
@@ -6,6 +6,8 @@
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private TaiKhoan user;
+        private Manager_Form main;
 
         /// <summary>
         /// Clean up any resources being used.
@@ -28,20 +30,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Reset_Pass_Form));
-            //
-            // ResetPassword
+            this.label_MkMoiLai = new System.Windows.Forms.Label();
+            this.textBox_MkMoiLai = new System.Windows.Forms.TextBox();
+            this.label_MkMoi = new System.Windows.Forms.Label();
+            this.textBox_MkMoi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_MkCu = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_XacNhan = new System.Windows.Forms.Button();
+            this.button_Huy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            // label_MkMoiLai
+            // 
+            this.label_MkMoiLai.AutoSize = true;
+            this.label_MkMoiLai.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_MkMoiLai.Location = new System.Drawing.Point(7, 169);
+            this.label_MkMoiLai.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_MkMoiLai.Name = "label_MkMoiLai";
+            this.label_MkMoiLai.Size = new System.Drawing.Size(204, 29);
+            this.label_MkMoiLai.TabIndex = 8;
+            this.label_MkMoiLai.Text = "Nhập lại mật khẩu";
+            // 
+            // textBox_MkMoiLai
+            // 
+            this.textBox_MkMoiLai.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_MkMoiLai.Location = new System.Drawing.Point(238, 166);
+            this.textBox_MkMoiLai.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox_MkMoiLai.Name = "textBox_MkMoiLai";
+            this.textBox_MkMoiLai.Size = new System.Drawing.Size(258, 35);
+            this.textBox_MkMoiLai.TabIndex = 7;
+            // 
+            // label_MkMoi
+            // 
+            this.label_MkMoi.AutoSize = true;
+            this.label_MkMoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_MkMoi.Location = new System.Drawing.Point(56, 126);
+            this.label_MkMoi.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_MkMoi.Name = "label_MkMoi";
+            this.label_MkMoi.Size = new System.Drawing.Size(155, 29);
+            this.label_MkMoi.TabIndex = 9;
+            this.label_MkMoi.Text = "Mật khẩu mới";
+            // 
+            // textBox_MkMoi
+            // 
+            this.textBox_MkMoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_MkMoi.Location = new System.Drawing.Point(238, 126);
+            this.textBox_MkMoi.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox_MkMoi.Name = "textBox_MkMoi";
+            this.textBox_MkMoi.Size = new System.Drawing.Size(258, 35);
+            this.textBox_MkMoi.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 85);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 29);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Mật khẩu cũ";
+            // 
+            // textBox_MkCu
+            // 
+            this.textBox_MkCu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_MkCu.Location = new System.Drawing.Point(238, 85);
+            this.textBox_MkCu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox_MkCu.Name = "textBox_MkCu";
+            this.textBox_MkCu.Size = new System.Drawing.Size(258, 35);
+            this.textBox_MkCu.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(152, 23);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 41);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Đổi Mật Khẩu";
+            // 
+            // button_XacNhan
+            // 
+            this.button_XacNhan.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_XacNhan.Location = new System.Drawing.Point(300, 252);
+            this.button_XacNhan.Name = "button_XacNhan";
+            this.button_XacNhan.Size = new System.Drawing.Size(133, 48);
+            this.button_XacNhan.TabIndex = 13;
+            this.button_XacNhan.Text = "Xác Nhận";
+            this.button_XacNhan.UseVisualStyleBackColor = true;
+            this.button_XacNhan.Click += new System.EventHandler(this.button_XacNhan_Click);
+            // 
+            // button_Huy
+            // 
+            this.button_Huy.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Huy.Location = new System.Drawing.Point(78, 252);
+            this.button_Huy.Name = "button_Huy";
+            this.button_Huy.Size = new System.Drawing.Size(133, 48);
+            this.button_Huy.TabIndex = 13;
+            this.button_Huy.Text = "Hủy";
+            this.button_Huy.UseVisualStyleBackColor = true;
+            this.button_Huy.Click += new System.EventHandler(this.button_Huy_Click);
+            // 
+            // Reset_Pass_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(832, 454);
+            this.ClientSize = new System.Drawing.Size(515, 334);
+            this.Controls.Add(this.button_Huy);
+            this.Controls.Add(this.button_XacNhan);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_MkCu);
+            this.Controls.Add(this.label_MkMoiLai);
+            this.Controls.Add(this.textBox_MkMoiLai);
+            this.Controls.Add(this.label_MkMoi);
+            this.Controls.Add(this.textBox_MkMoi);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Reset_Pass_Form";
             this.Text = "Reset Password";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Label label_MkMoiLai;
+        private System.Windows.Forms.TextBox textBox_MkMoiLai;
+        private System.Windows.Forms.Label label_MkMoi;
+        private System.Windows.Forms.TextBox textBox_MkMoi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_MkCu;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_XacNhan;
+        private System.Windows.Forms.Button button_Huy;
     }
 }
--- HotelBookingManagement/HotelBookingManagement/Presentation Layers/Reset_Pass_Form.cs
+++ HotelBookingManagement/HotelBookingManagement/Presentation Layers/Reset_Pass_Form.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelBookingManagement.Data_Access_Layers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,58 @@ namespace HotelBookingManagement
 {
     public partial class Reset_Pass_Form : Form
     {
-        public Reset_Pass_Form()
+        public Reset_Pass_Form(Manager_Form m, TaiKhoan user)
         {
             InitializeComponent();
+            this.main = m;
+            this.user = user;
+        }
+
+        private void button_XacNhan_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.textBox_MkCu.Text != null)
+                {
+                    if (TaiKhoan_DAL.Instance.CheckMatKhau(user.tenDN, TaiKhoan.encode(this.textBox_MkCu.Text)))
+                    {
+                        if (this.textBox_MkMoi != null)
+                        {
+                            if (this.textBox_MkMoiLai.Text != null)
+                            {
+                                if (this.textBox_MkMoiLai.Text == this.textBox_MkMoi.Text)
+                                {
+                                    if (TaiKhoan_DAL.Instance.updateTaiKhoan(this.user.tenDN, TaiKhoan.encode(this.textBox_MkMoi.Text)))
+                                        if (MessageBox.Show("Thay đổi mật khẩu thành công\ntiến hành đăng xuất để đăng nhập lại", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information) == DialogResult.OK)
+                                            main.logout(sender, e);
+                                }
+                                else
+                                {
+                                    if (MessageBox.Show("Mật khẩu nhập lại không đúng", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning) == DialogResult.OK)
+                                        this.textBox_MkMoiLai.Clear();
+                                }
+                            }
+                            else
+                                MessageBox.Show("Vui lòng nhập lại mật khẩu", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                        MessageBox.Show("Vui lòng nhập mật khẩu mới", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    else
+                        MessageBox.Show("Mật khẩu cũ không đúng", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);   
+                }
+                else
+                    MessageBox.Show("vui lòng nhập mật khẩu cũ", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+        }
+
+        private void button_Huy_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc muốn hủy thay đổi mật khẩu", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                this.Close();
         }
     }
 }
